3cdaa64d752a3e6dc82b7bb326c92aa67ac837d4
Mark Mulder <markmulder@gmail.com>
2017-03-12 16:40:20 +0100

    wip

--- scale.sh
+++ scale.sh
@@ -1,1 +1,2 @@
 ffmpeg -i cmd-shift-g-open-dialog.mov -c:v libx264 -preset slow -crf 22 -vf scale=1280:800 output-scaled.mkv
+ffmpeg -i cmd-shift-g-open-dialog.mov -c:v libx264 -preset slow -crf 22 -vf scale=1280:800 output-scaled.mp4
